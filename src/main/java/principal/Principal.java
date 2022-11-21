76d2a2c0dee51e0bc06b2f3e82e0bb809b2d768e
Escoz-Portatil <kyrovilches@gmail.com>
2022-11-21 20:48:30 +0100

    Interfaz en proceso

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,9 +1,14 @@
 package principal;
 
+import com.formdev.flatlaf.FlatLightLaf;
+import interfaz.VistaPrincipal;
+
 public class Principal {
 
-	public static void main(String[] args) {
-
-	}
+    public static void main(String[] args) {
+        
+        FlatLightLaf.setup();
+        new VistaPrincipal().setVisible(true);
+    }
 
 }
